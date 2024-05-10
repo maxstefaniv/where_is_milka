6ba46b9dddce36cc416578ce67df21ed6e122cb7
Max Stefaniv <max.stefaniv@elastacloud.com>
2024-05-10 20:17:47 +0200

    did the todos so off we go

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+
+
+
+# TODO 1: create service to find my webcam in the network
+# TODO 2: connect to webcam with env variables
+# TODO 3: get image from webcam
+    # TODO 3.1: check if have cat in the image
+# TODO 4: Look around if can find cat
